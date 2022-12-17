50ba194163ae0879b0ee28cb899385ca91fcdb44
KevJord76 <kevjord76@gmail.com>
2022-12-17 22:11:02 +0000

    feat: Add validate_choice function

--- run.py
+++ run.py
@@ -3,10 +3,10 @@ categories = ["Sport","Music","Movies"]
 
 def display_menu():
     """
-    # Display the main menu
+    # Display the hangman game main menu
     """
     print("-" * 49)
-    print("Welcome to the Guess Famous Person Hangman Game")
+    print("Welcome to Guess the Famous Person Hangman Game")
     print("-" * 49)
     print("\nPlease select a Category:")
 
@@ -17,14 +17,43 @@ def display_menu():
     # Allow the user to exit the game
     print("(99 to exit)\n")
 
+def validate_choice(chosen):
+    """
+    Validate the menu choice made by the user:
+    Inside the try, converts string into an integer
+    Raises valueError if string cannot be converted into an integer 
+    or if the choice is not within the menu parameters or 99
+    """
+    try:
+        chosen = int(chosen)
+        if not chosen in range(1,len(categories)+1) and not chosen == 99:
+            raise ValueError("Incorrect menu option")
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again:\n")   
+        return False
+
+    return True   
+
 def main():
     """
     Run all program functions
     """
     display_menu()
 
-    # Get the user's choice
-    choice = input("Please enter your choice:\n")
-
+    # Get the user's menu choice
+    while True:        
+        choice = input("Please enter your choice:\n")
+        if validate_choice(choice):
+            # Convert to an integer
+            choice = int(choice)
+            # Exit the game
+            if choice == 99:
+                print("Bye, bye, please play again soon!")
+                exit() 
+            else:
+                # Valid menu option chosen
+                print(f"You chose option no {choice}. {categories[choice-1]}")  
+                break   
+                      
 # Call the main function
 main()
