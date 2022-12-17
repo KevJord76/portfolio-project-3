3f45594140f5730790272ceebb2d732ceafbcd60
KevJord76 <kevjord76@gmail.com>
2022-12-17 19:04:59 +0000

    feat: Add hangman game main menu

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Hangman game categories
+categories = ["Sport","Music","Movies"]
+
+# Display the main menu
+print("-" * 49)
+print("Welcome to the Guess Famous Person Hangman Game")
+print("-" * 49)
+print("\nPlease select a Category:")
+
+# Display the hangman game categories
+for i in range(len(categories)):
+    print(str(i+1)+". ", categories[i])  
+
+# Allow the user to exit the game
+print("(99 to exit)\n")
+
+# Get the user's choice
+choice = input("Please enter your choice:\n")
