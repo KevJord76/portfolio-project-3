38e9a39c7d16e5834fcd4a0e744d54fe962f7ed4
KevJord76 <kevjord76@gmail.com>
2022-12-17 19:21:33 +0000

    refactor: Add main, display_menu functions

--- run.py
+++ run.py
@@ -1,18 +1,30 @@
 # Hangman game categories
 categories = ["Sport","Music","Movies"]
 
-# Display the main menu
-print("-" * 49)
-print("Welcome to the Guess Famous Person Hangman Game")
-print("-" * 49)
-print("\nPlease select a Category:")
+def display_menu():
+    """
+    # Display the main menu
+    """
+    print("-" * 49)
+    print("Welcome to the Guess Famous Person Hangman Game")
+    print("-" * 49)
+    print("\nPlease select a Category:")
 
-# Display the hangman game categories
-for i in range(len(categories)):
-    print(str(i+1)+". ", categories[i])  
+    # Display the hangman game categories
+    for i in range(len(categories)):
+        print(str(i+1)+". ", categories[i])  
 
-# Allow the user to exit the game
-print("(99 to exit)\n")
+    # Allow the user to exit the game
+    print("(99 to exit)\n")
 
-# Get the user's choice
-choice = input("Please enter your choice:\n")
+def main():
+    """
+    Run all program functions
+    """
+    display_menu()
+
+    # Get the user's choice
+    choice = input("Please enter your choice:\n")
+
+# Call the main function
+main()
