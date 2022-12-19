23d4996e0d9a1b9dbe4206e39e4c5f70a2f1c95c
KevJord76 <kevjord76@gmail.com>
2022-12-19 20:01:43 +0000

    feat: Add while loop game play win/lose criteria to play game function

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+# Import the random library
 import random
 
 # Hangman game categories
@@ -8,7 +9,7 @@ sport = {
     'Lionel Messi': 'Argentine football player',
     'Serena Williams': 'American tennis player',
     'LeBron James': 'American basketball player',
-    'Lydia Ko': 'New Zealand golf player',
+    'Lydia Ko': 'New Zealand golfer',
     'Max Verstappen': 'Belgian-Dutch motorsports racing driver'
 }
 
@@ -31,7 +32,7 @@ def display_menu():
 def validate_choice(chosen):
     """
     Validate the menu choice made by the user:
-    Inside the try, converts string into an integer
+    Inside the try, converts chosen string into an integer
     Raises valueError if string cannot be converted into an integer 
     or if the choice is not within the menu parameters or 99
     """
@@ -64,19 +65,66 @@ def play_game(game_name_and_clue):
     name = game_name_and_clue[0].upper()
     clue = game_name_and_clue[1]
 
+    # Store the right and wrong guessed characters
+    right_guesses = ""
+    wrong_guesses = ""
+    
+    # This is the number of wrong guesses the user is allowed 
+    life_lines = 6
+
     # Inform the user
     print("\nCan you guess the famous person below?\n")
 
-    # Print underscores and spaces to denote the characters in the name 
-    for char in name:
-        if char != " ":
-            print("_", end=' ')
-        else:
-            print(char, end=' ')
-    print("\n")
+    # As long as the user has life lines left, keep playing
+    while life_lines > 0: 
+        # If this stays at True the user has won
+        winner = True
+        # Print underscores and spaces to denote the characters in the name or else print the valid characters 
+        for char in name:
+            if (char != " ") and (char not in right_guesses):
+                print("_", end=' ')
+                # Still letter(s) to be found 
+                winner = False
+            else:
+                print(char, end=' ')
+        print("\n")
 
-    # Ask the user for a character
-    character = input("Please enter a character:\n")
+        # Has the user won?
+        if winner:
+            # This game has ended with a win
+            print(f"Congrats! You have won the game! You have guessed the famous person: {name}")
+            break
+
+        # Ask the user for a character
+        character = input("Please enter a character:\n")
+        character = character.upper()
+        if (character in right_guesses) or (character in wrong_guesses):
+            print("You have already guessed this letter, please try again:")
+        else:
+            # Is the character in the name?
+            if character in name:
+                print("Well done! This letter is in the famous person's name")
+                right_guesses += character
+            else:
+                print("Hard luck, this letter is not in the famous person's name")
+                wrong_guesses += character
+                life_lines -= 1
+                if life_lines == 1:
+                    # Ask the user if they want to see a clue
+                    print(f"You have only {life_lines} life line left...\n")
+                    see_clue = input("Do you want to see a clue? Y/N:\n")
+                    see_clue = see_clue.upper()
+                    # Display a clue for the user
+                    if see_clue == "Y":
+                        print(clue)
+                elif life_lines == 0:
+                    # This game has ended with a loss
+                    print("Sorry you are out of life lines! You have lost this game...\n")
+                else:
+                    print(f"You have {life_lines} life lines left...\n")
+
+        
+
     
 def main():
     """
